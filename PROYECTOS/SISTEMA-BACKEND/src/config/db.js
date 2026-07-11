68063db8c6a977f3557ab02b8fb728b5472ddd5d
Styx117 <Styx117@outlook.com.pe>
2026-07-11 02:01:40 -0500

    Co-authored-by: Alexndr20s <Alexndr20s@users.noreply.github.com> Co-authored-by: Moon26sc <Moon26sc@users.noreply.github.com>

--- PROYECTOS/SISTEMA-BACKEND/src/config/db.js
+++ PROYECTOS/SISTEMA-BACKEND/src/config/db.js
@@ -3,16 +3,19 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   ssl: {
-    require: true,
     rejectUnauthorized: false
   }
 });
-
+//Ya bueno, modifiqué un toque el código para que se conecte a la base de datos en AWS RDS. 
+// Ahora debería funcionar correctamente. OKA 
+//Primero, ya vi que has escrito horrible. Segundo, si
+//DARIO LO TIENES CORRIENDO RIGHT?
+//COMO QUE ESCRIBÍ HORRIBLE? ESPEREN CREE SALA PORQUE ME ANDO ENREDANDO
 pool.connect((err, client, release) => {
   if (err) {
     return console.error('Error adquiriendo el cliente de la base de datos:', err.stack);
