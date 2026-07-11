fdaffe38f4454b318a627629b7d5a2e25298b7b1
Styx117 <Styx117@outlook.com.pe>
2026-07-11 02:08:35 -0500

    correcciones...

--- PROYECTOS/SISTEMA-BACKEND/src/routes/clienteRoutes.js
+++ PROYECTOS/SISTEMA-BACKEND/src/routes/clienteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {  obtenerClientes, crearCliente, eliminarCliente } = require('../controllers/ClienteController');
+const {  obtenerClientes, crearCliente, eliminarCliente } = require('../controllers/clienteController');
 
 router.get('/', obtenerClientes);
 router.post('/', crearCliente);
